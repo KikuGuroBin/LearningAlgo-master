d644b846b44a275c153bba9f1c06fc14eca03b6b
龍生 小林 <kss-15180151@c-league.jp>
2017-11-08 16:20:26 +0900

    テストページ追加

---
+++ LearningAlgo/LearningAlgo/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LearningAlgo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            // 画面遷移
+            Navigation.PushAsync(new Test1());
+		}
+	}
+}
