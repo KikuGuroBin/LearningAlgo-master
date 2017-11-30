104cecf01be24d85dd76d51f090291c13db5f80b
KikuGuroBin <ripobitan.p.314159@gmail.com>
2017-11-30 16:23:31 +0900

    レイアウト部分の結合(未完)        FlowPage        ImitationDialog        MyImage        ImageDrugEventArgs

--- LearningAlgo/LearningAlgo/KeyboardDialog.xaml.cs
+++ LearningAlgo/LearningAlgo/KeyboardDialog.xaml.cs
@@ -6,9 +6,27 @@ namespace LearningAlgo
 {
     public partial class KeyboardDialog : ContentView
     {
+        /// <summary>
+        /// Labelのデータバインディング用
+        /// </summary>
+        private ViewBounds LabelBounds;
+
         public KeyboardDialog()
         {
             InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            LabelBounds = new ViewBounds
+            {
+                X = width / 2 - 30,
+                Y = height / 2,
+            };
+
+            //Label1.BindingContext = LabelBounds;
         }
     }
 }
