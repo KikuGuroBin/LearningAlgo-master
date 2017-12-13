5be68cf316b8d0c7fb2f3698d32ec40886757523
KikuGuroBin <ripobitan.p.314159@gmail.com>
2017-12-13 12:25:05 +0900

    テストデータ更新

--- LearningAlgo/LearningAlgo/DBConnection.cs
+++ LearningAlgo/LearningAlgo/DBConnection.cs
@@ -27,8 +27,8 @@ namespace LearningAlgo
                 /*flowデータ*/
                 connection.Insert(new FlowTable { flow_id = "1", flow_name = "1", comment = "test" });
                 connection.Insert(new FlowTable { flow_id = "2", flow_name = "2", comment = "quizeTest" });
-
-
+                connection.Insert(new FlowTable { flow_id = "3", flow_name = "3", comment = "test2" });
+                connection.Insert(new FlowTable { flow_id = "4", flow_name = "4", comment = "test3" });
 
                 /*flowPartsデータ*/
                 connection.Insert(new FlowPartsTable { flow_id = "1", identification_id = "1", type_id = "SideSikaku.png", data = "1→i", position_X = "10", position_Y = "70", startFlag = "1" });
@@ -38,27 +38,27 @@ namespace LearningAlgo
                 connection.Insert(new FlowPartsTable { flow_id = "1", identification_id = "5", type_id = "SideHisigata.png", data = "i≦3", position_X = "10", position_Y = "350", startFlag = "0" });
                 connection.Insert(new FlowPartsTable { flow_id = "1", identification_id = "6", type_id = "SideHeikou.png", data = "j出力", position_X = "10", position_Y = "420", startFlag = "0" });
 
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "1", type_id = "SideSikaku.png", data = "1→i", position_X = "10", position_Y = "70", startFlag = "1" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "2", type_id = "SideSikaku.png", data = "2→j", position_X = "10", position_Y = "140", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "3", type_id = "SideSikaku.png", data = "j＋1→j", position_X = "10", position_Y = "210", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "4", type_id = "SideSikaku.png", data = "i＋1→i", position_X = "10", position_Y = "280", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "5", type_id = "SideHisigata.png", data = "i≦3", position_X = "10", position_Y = "350", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "6", type_id = "SideHeikou.png", data = "j出力", position_X = "10", position_Y = "420", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "1", type_id = "SideSikaku.png", data = "1→i", position_X = "10", position_Y = "70", startFlag = "1" });
+                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "2", type_id = "SideSikaku.png", data = "2→j", position_X = "10", position_Y = "140", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "3", type_id = "SideSikaku.png", data = "j＋1→j", position_X = "10", position_Y = "210", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "4", type_id = "SideSikaku.png", data = "i＋1→i", position_X = "10", position_Y = "280", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "5", type_id = "SideHisigata.png", data = "i≦3", position_X = "10", position_Y = "350", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "6", type_id = "SideHeikou.png", data = "j出力", position_X = "10", position_Y = "420", startFlag = "0" });
 
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "1", type_id = "SideSikaku.png", data = "1→i", position_X = "10", position_Y = "70", startFlag = "1" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "2", type_id = "SideSikaku.png", data = "2→j", position_X = "10", position_Y = "140", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "3", type_id = "SideSikaku.png", data = "j＋1→j", position_X = "10", position_Y = "210", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "4", type_id = "SideSikaku.png", data = "i＋1→i", position_X = "10", position_Y = "280", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "5", type_id = "SideHisigata.png", data = "i≦5", position_X = "10", position_Y = "350", startFlag = "0" });
-                //connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "6", type_id = "SideHeikou.png", data = "j出力", position_X = "10", position_Y = "420", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "3", identification_id = "1", type_id = "SideSikaku.png", data = "1→i", position_X = "10", position_Y = "70", startFlag = "1" });
+                connection.Insert(new FlowPartsTable { flow_id = "3", identification_id = "2", type_id = "SideSikaku.png", data = "2→j", position_X = "10", position_Y = "140", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "3", identification_id = "3", type_id = "SideSikaku.png", data = "j＋1→j", position_X = "10", position_Y = "210", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "3", identification_id = "4", type_id = "SideSikaku.png", data = "i＋1→i", position_X = "10", position_Y = "280", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "3", identification_id = "5", type_id = "SideHisigata.png", data = "i≦5", position_X = "10", position_Y = "350", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "3", identification_id = "6", type_id = "SideHeikou.png", data = "j出力", position_X = "10", position_Y = "420", startFlag = "0" });
 
-                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "1", type_id = "SideSikaku.png", data = "1→i", position_X = "130", position_Y = "70", startFlag = "1" });
-                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "2", type_id = "SideSikaku.png", data = "2→j", position_X = "130", position_Y = "140", startFlag = "0" });
-                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "3", type_id = "SideHisigata.png", data = "i＜3", position_X = "130", position_Y = "210", startFlag = "0" });
-                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "4", type_id = "SideHeikou.png", data = "j出力", position_X = "30", position_Y = "210", startFlag = "0" });
-                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "5", type_id = "SideSikaku.png", data = "i＋1→i", position_X = "130", position_Y = "280", startFlag = "0" });
-                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "6", type_id = "SideSikaku.png", data = "j＋1→j", position_X = "130", position_Y = "350", startFlag = "0" });
-                connection.Insert(new FlowPartsTable { flow_id = "2", identification_id = "7", type_id = "SideHisigata.png", data = "j＜3", position_X = "130", position_Y = "420", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "4", identification_id = "1", type_id = "SideSikaku.png", data = "1→i", position_X = "130", position_Y = "70", startFlag = "1" });
+                connection.Insert(new FlowPartsTable { flow_id = "4", identification_id = "2", type_id = "SideSikaku.png", data = "2→j", position_X = "130", position_Y = "140", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "4", identification_id = "3", type_id = "SideHisigata.png", data = "i＜3", position_X = "130", position_Y = "210", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "4", identification_id = "4", type_id = "SideHeikou.png", data = "j出力", position_X = "30", position_Y = "210", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "4", identification_id = "5", type_id = "SideSikaku.png", data = "i＋1→i", position_X = "130", position_Y = "280", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "4", identification_id = "6", type_id = "SideSikaku.png", data = "j＋1→j", position_X = "130", position_Y = "350", startFlag = "0" });
+                connection.Insert(new FlowPartsTable { flow_id = "4", identification_id = "7", type_id = "SideHisigata.png", data = "j＜3", position_X = "130", position_Y = "420", startFlag = "0" });
 
 
                 /*outputデータ*/
@@ -70,30 +70,30 @@ namespace LearningAlgo
                 connection.Insert(new OutputTable { flow_id = "1", identification_id = "5", output_identification_id = "6", blanch_flag = "-1" });
                 connection.Insert(new OutputTable { flow_id = "1", identification_id = "6", output_identification_id = "-1", blanch_flag = "0" });
 
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "1", output_identification_id = "2", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "2", output_identification_id = "3", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "3", output_identification_id = "4", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "4", output_identification_id = "5", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "5", output_identification_id = "3", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "5", output_identification_id = "6", blanch_flag = "-1" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "6", output_identification_id = "-1", blanch_flag = "0" });
-
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "1", output_identification_id = "2", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "2", output_identification_id = "3", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "3", output_identification_id = "4", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "4", output_identification_id = "5", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "5", output_identification_id = "3", blanch_flag = "0" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "5", output_identification_id = "6", blanch_flag = "-1" });
-                //connection.Insert(new OutputTable { flow_id = "2", identification_id = "6", output_identification_id = "-1", blanch_flag = "0" });
-
                 connection.Insert(new OutputTable { flow_id = "2", identification_id = "1", output_identification_id = "2", blanch_flag = "0" });
                 connection.Insert(new OutputTable { flow_id = "2", identification_id = "2", output_identification_id = "3", blanch_flag = "0" });
-                connection.Insert(new OutputTable { flow_id = "2", identification_id = "3", output_identification_id = "5", blanch_flag = "0" });
-                connection.Insert(new OutputTable { flow_id = "2", identification_id = "3", output_identification_id = "4", blanch_flag = "-1" });
-                connection.Insert(new OutputTable { flow_id = "2", identification_id = "5", output_identification_id = "6", blanch_flag = "0" });
-                connection.Insert(new OutputTable { flow_id = "2", identification_id = "6", output_identification_id = "7", blanch_flag = "0" });
-                connection.Insert(new OutputTable { flow_id = "2", identification_id = "7", output_identification_id = "3", blanch_flag = "-1" });
-                connection.Insert(new OutputTable { flow_id = "2", identification_id = "7", output_identification_id = "6", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "2", identification_id = "3", output_identification_id = "4", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "2", identification_id = "4", output_identification_id = "5", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "2", identification_id = "5", output_identification_id = "3", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "2", identification_id = "5", output_identification_id = "6", blanch_flag = "-1" });
+                connection.Insert(new OutputTable { flow_id = "2", identification_id = "6", output_identification_id = "-1", blanch_flag = "0" });
+
+                connection.Insert(new OutputTable { flow_id = "3", identification_id = "1", output_identification_id = "2", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "3", identification_id = "2", output_identification_id = "3", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "3", identification_id = "3", output_identification_id = "4", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "3", identification_id = "4", output_identification_id = "5", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "3", identification_id = "5", output_identification_id = "3", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "3", identification_id = "5", output_identification_id = "6", blanch_flag = "-1" });
+                connection.Insert(new OutputTable { flow_id = "3", identification_id = "6", output_identification_id = "-1", blanch_flag = "0" });
+
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "1", output_identification_id = "2", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "2", output_identification_id = "3", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "3", output_identification_id = "5", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "3", output_identification_id = "4", blanch_flag = "-1" });
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "5", output_identification_id = "6", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "6", output_identification_id = "7", blanch_flag = "0" });
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "7", output_identification_id = "3", blanch_flag = "-1" });
+                connection.Insert(new OutputTable { flow_id = "4", identification_id = "7", output_identification_id = "6", blanch_flag = "0" });
 
 
                 /*typeデータ*/
